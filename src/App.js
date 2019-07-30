26ec11f0469373277f00e506377d2c0c3b5357bd
Agbele_Kolade <kolagbele@gmail.com>
2019-07-30 14:26:21 +0200

    eslint set up
    - setup lint rules with eslint
    - set up formatter with prettier

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Welcome to my blog
-    </div>
-  );
+	return (
+		<div>
+			Welcome to my blog
+		</div>
+	);
 }
 
 export default App;
