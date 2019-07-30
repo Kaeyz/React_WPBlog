3c5185f758a4fb615ff50351b84d39eb526e9dfe
Agbele_Kolade <kolagbele@gmail.com>
2019-07-30 15:51:16 +0200

    Blog list component created
    - created the blog list compnent and hooked it up with the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
+import BlogList from '../components/BlogList';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div>
 				<Header />
+				<BlogList />
 				<Footer />
 			</div>
 		);
