d088de2a9e2a0d8bb3861e203a9a8b4af65311d5
Agbele_Kolade <kolagbele@gmail.com>
2019-07-30 15:41:07 +0200

    Footer component created
    - footer component created and added to the home page

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+	return (
+		<div className='container-fluid bg-info footer'>
+			<h6>Copyright 2019</h6>
+		</div>
+	);
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 
 export default class Home extends Component {
@@ -8,6 +9,7 @@ export default class Home extends Component {
 		return (
 			<div>
 				<Header />
+				<Footer />
 			</div>
 		);
 	}
