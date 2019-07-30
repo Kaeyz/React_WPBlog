835a08079a94409365b0d41389d0f17d3d6746cd
Agbele_Kolade <kolagbele@gmail.com>
2019-07-30 18:03:28 +0200

    made blog list responsive using using css grid

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 export default class BlogList extends Component {
 	render() {
 		return (
-			<div className='container'>
+			<div className='container blog'>
 
+				<div className="card">
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>
+					</div>
+				</div>
+			
 				<div className="card">
 					<img src="..." className="card-img-top" alt="..." />
 					<div className="card-body">
@@ -20,23 +28,7 @@ export default class BlogList extends Component {
 						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>
 					</div>
 				</div>
-
-				<div className="card">
-					<img src="..." className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>
-					</div>
-				</div>
-
-				<div className="card">
-					<img src="..." className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>
-					</div>
-				</div>
-
+				
 				<div className="card">
 					<img src="..." className="card-img-top" alt="..." />
 					<div className="card-body">
